8e398d5bf59d7e83843e4e75956fb56746c246b8
Ilya Borisov <Klaster1@gmail.com>
2017-07-12 11:12:15 +0700

    remove summary screen

--- modules/web-console/frontend/app/modules/states/configuration.state.js
+++ modules/web-console/frontend/app/modules/states/configuration.state.js
@@ -21,17 +21,13 @@ import angular from 'angular';
 import previewPanel from './configuration/preview-panel.directive.js';
 
 // Summary screen.
-import ConfigurationSummaryCtrl from './configuration/summary/summary.controller';
 import ConfigurationResource from './configuration/Configuration.resource';
-import summaryTabs from './configuration/summary/summary-tabs.directive';
 import IgniteSummaryZipper from './configuration/summary/summary-zipper.service';
 
 import clustersTpl from 'views/configuration/clusters.tpl.pug';
 import cachesTpl from 'views/configuration/caches.tpl.pug';
 import domainsTpl from 'views/configuration/domains.tpl.pug';
 import igfsTpl from 'views/configuration/igfs.tpl.pug';
-import summaryTpl from 'views/configuration/summary.tpl.pug';
-import summaryTabsTemplateUrl from 'views/configuration/summary-tabs.pug';
 
 import clustersCtrl from 'Controllers/clusters-controller';
 import domainsCtrl from 'Controllers/domains-controller';
@@ -42,14 +38,9 @@ import base2 from 'views/base2.pug';
 
 angular.module('ignite-console.states.configuration', ['ui.router'])
     .directive(...previewPanel)
-    // Summary screen
-    .directive(...summaryTabs)
     // Services.
     .service('IgniteSummaryZipper', IgniteSummaryZipper)
     .service('IgniteConfigurationResource', ConfigurationResource)
-    .run(['$templateCache', ($templateCache) => {
-        $templateCache.put('summary-tabs.html', summaryTabsTemplateUrl);
-    }])
     // Configure state provider.
     .config(['$stateProvider', ($stateProvider) => {
         // Setup the states.
@@ -152,15 +143,5 @@ angular.module('ignite-console.states.configuration', ['ui.router'])
                 },
                 controller: igfsCtrl,
                 controllerAs: '$ctrl'
-            })
-            .state('base.configuration.tabs.advanced.summary', {
-                url: '/summary?{clusterID:string}',
-                templateUrl: summaryTpl,
-                permission: 'configuration',
-                controller: ConfigurationSummaryCtrl,
-                controllerAs: 'ctrl',
-                tfMetaTags: {
-                    title: 'Configurations Summary'
-                }
             });
     }]);
