0b22cb24bec41fa99963808d3b969828566c18bd
Ilya Borisov <Klaster1@gmail.com>
2017-07-18 16:56:36 +0700

    fix forumutils ensure visible

--- modules/web-console/frontend/app/services/FormUtils.service.js
+++ modules/web-console/frontend/app/services/FormUtils.service.js
@@ -18,7 +18,7 @@
 export default ['IgniteFormUtils', ['$window', 'IgniteFocus', ($window, Focus) => {
     function ensureActivePanel(ui, pnl, focusId) {
         if (ui && ui.loadPanel) {
-            const collapses = $('div.panel-collapse');
+            const collapses = $('[bs-collapse-target]');
 
             ui.loadPanel(pnl);
 
