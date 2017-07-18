f1c1a9a3895873085eca025112eef2c0080bbc5e
Ilya Borisov <Klaster1@gmail.com>
2017-07-18 16:59:38 +0700

    sync selected row to table

--- modules/web-console/frontend/app/components/page-configure/components/pc-items-table/component.js
+++ modules/web-console/frontend/app/components/page-configure/components/pc-items-table/component.js
@@ -30,6 +30,7 @@ export default {
         onAction: '&',
         columnDefs: '<',
         tableTitle: '<',
-        immediateEdit: '<?'
+        immediateEdit: '<?',
+        selectedRowId: '<?'
     }
 };
