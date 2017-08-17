58204a6ea40cc6da9a6aeb2cbe68e17e434da30b
Ilya Borisov <Klaster1@gmail.com>
2017-08-17 15:47:27 +0700

    refactor ignite-unique validator make it usable outside of ng-repeat

--- modules/web-console/frontend/app/modules/form/validator/unique.directive.js
+++ modules/web-console/frontend/app/modules/form/validator/unique.directive.js
@@ -16,39 +16,59 @@
  */
 
 import isNumber from 'lodash/fp/isNumber';
+import get from 'lodash/fp/get';
 
-export default ['igniteUnique', ['$parse', ($parse) => {
-    const link = (scope, el, attrs, [ngModel]) => {
-        if (_.isUndefined(attrs.igniteUnique) || !attrs.igniteUnique)
-            return;
+class Controller {
+    static $inject = ['$scope'];
 
-        const isNew = _.startsWith(attrs.name, 'new');
-        const property = attrs.igniteUniqueProperty;
+    constructor($scope) {
+        this.$scope = $scope;
+    }
 
-        ngModel.$validators.igniteUnique = (value) => {
-            const arr = $parse(attrs.igniteUnique)(scope);
+    $onInit() {
+        const isNew = this.key && this.key.startsWith('new');
+        const shouldNotSkip = (item) => get(this.skip[0], item) !== get(...this.skip);
 
-            // Return true in case if array not exist, array empty.
-            if (!arr || !arr.length)
-                return true;
+        this.ngModel.$validators.igniteUnique = (value) => {
+            const matches = (item) => (this.key ? item[this.key] : item) === value;
 
-            const idx = _.findIndex(arr, (item) => (property ? item[property] : item) === value);
+            if (!this.skip) {
+                // Return true in case if array not exist, array empty.
+                if (!this.items || !this.items.length) return true;
 
-            // In case of new element check all items.
-            if (isNew)
-                return idx < 0;
+                const idx = this.items.findIndex(matches);
 
-            // Case for new component list editable.
-            const $index = isNumber(scope.$index) ? scope.$index : scope.$parent.$index;
+                // In case of new element check all items.
+                if (isNew) return idx < 0;
 
-            // Check for $index in case of editing in-place.
-            return (_.isNumber($index) && (idx < 0 || $index === idx));
+                // Case for new component list editable.
+                const $index = isNumber(this.$scope.$index) ? this.$scope.$index : this.$scope.$parent.$index;
+
+                // Check for $index in case of editing in-place.
+                return (isNumber($index) && (idx < 0 || $index === idx));
+            }
+            // TODO: converge both branches, use $index as idKey
+            return !(this.items || []).filter(shouldNotSkip).some(matches);
         };
-    };
+    }
 
+    $onChanges(changes) {
+        if ('items' in changes && changes.items.currentValue !== changes.items.previousValue)
+            this.ngModel.$validate();
+
+    }
+}
+
+export default ['igniteUnique', () => {
     return {
-        restrict: 'A',
-        link,
-        require: ['ngModel']
+        controller: Controller,
+        require: {
+            ngModel: 'ngModel'
+        },
+        bindToController: {
+            items: '<igniteUnique',
+            key: '@?igniteUniqueProperty',
+            skip: '<?igniteUniqueSkip'
+        }
     };
-}]];
+}];
