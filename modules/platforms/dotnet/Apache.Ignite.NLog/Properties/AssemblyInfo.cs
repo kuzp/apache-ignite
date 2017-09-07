b577b71180c8345f40f533d54917a65ba6db65b0
Ignite Teamcity <ignite@apache.org>
2017-09-07 13:51:14 +0000

    version changed to: 2.1.3-p2

--- modules/platforms/dotnet/Apache.Ignite.NLog/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.NLog/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c6b58e4a-a2e9-4554-ad02-68ce6da5cfb7")]
 
-[assembly: AssemblyVersion("2.2.0.22385")]
-[assembly: AssemblyFileVersion("2.2.0.22385")]
-[assembly: AssemblyInformationalVersion("2.2.0")]
+[assembly: AssemblyVersion("2.1.3.22790")]
+[assembly: AssemblyFileVersion("2.1.3.22790")]
+[assembly: AssemblyInformationalVersion("2.1.3-p2")]
 
 [assembly: CLSCompliant(true)]
