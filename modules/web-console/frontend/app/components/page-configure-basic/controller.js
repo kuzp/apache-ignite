d4d0271a38c350ef354bae9c68b04046a19c6f02
Ilya Borisov <Klaster1@gmail.com>
2017-09-07 16:34:14 +0700

    add changes guard and updated validation to basic config

--- modules/web-console/frontend/app/components/page-configure-basic/controller.js
+++ modules/web-console/frontend/app/components/page-configure-basic/controller.js
@@ -19,24 +19,29 @@ import 'rxjs/add/operator/map';
 import cloneDeep from 'lodash/cloneDeep';
 
 export default class PageConfigureBasicController {
-    static $inject = ['Clusters', 'Caches', 'IgniteVersion', '$element'];
+    static $inject = [
+        'Clusters', 'Caches', 'IgniteVersion', '$element', 'ConfigChangesGuard', 'IgniteFormUtils', '$scope'
+    ];
 
-    constructor(Clusters, Caches, IgniteVersion, $element) {
-        Object.assign(this, {Clusters, Caches, IgniteVersion, $element});
+    constructor(Clusters, Caches, IgniteVersion, $element, ConfigChangesGuard, IgniteFormUtils, $scope) {
+        Object.assign(this, {Clusters, Caches, IgniteVersion, $element, ConfigChangesGuard, IgniteFormUtils, $scope});
     }
 
     $postLink() {
         this.$element.addClass('panel--ignite');
     }
 
+    uiCanExit() {
+        if (this.form.$invalid) {
+            this.IgniteFormUtils.triggerValidation(this.form, this.$scope);
+            return false;
+        }
+        return this.ConfigChangesGuard.guard({cluster: this.clonedCluster});
+    }
+
     $onInit() {
         this.memorySizeInputVisible$ = this.IgniteVersion.currentSbj
             .map((version) => this.IgniteVersion.since(version.ignite, '2.0.0'));
-
-        // this.removeChangesGuard = this.ConfigurationChangesGuard.install({
-        //     fromState: 'base.configuration.edit.basic',
-        //     getItems: () => ([this.original])
-        // })
 
         this.formActionsMenu = [
             {
@@ -52,10 +57,6 @@ export default class PageConfigureBasicController {
         ];
     }
 
-    $onDestroy() {
-        // this.removeChangesGuard();
-    }
-
     addCache() {
         this.onItemAdd({$event: {type: 'caches'}});
     }
@@ -69,7 +70,7 @@ export default class PageConfigureBasicController {
     }
 
     save(andDownload = false) {
-        if (this.form.$invalid) return;
+        if (this.form.$invalid) return this.IgniteFormUtils.triggerValidation(this.form, this.$scope);
         this.onBasicSave({$event: {andDownload, cluster: cloneDeep(this.clonedCluster)}});
     }
 
