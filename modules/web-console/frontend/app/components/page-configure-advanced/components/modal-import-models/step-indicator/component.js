fae912f335c373a6c0926aa0632ae6dc9e4e53d0
Ilya Borisov <Klaster1@gmail.com>
2017-09-21 11:58:04 +0700

    finish steps indicator

--- modules/web-console/frontend/app/components/page-configure-advanced/components/modal-import-models/step-indicator/component.js
+++ modules/web-console/frontend/app/components/page-configure-advanced/components/modal-import-models/step-indicator/component.js
@@ -1,15 +1,16 @@
 import template from './template.pug';
 import './style.scss';
 
-// export class ModalImportModelsStepIndicator {
-//     $onInit() {
-
-//     }
-// }
+export class ModalImportModelsStepIndicator {
+    isVisited(index) {
+        return index <= this.steps.findIndex((step) => step.value === this.currentStep);
+    }
+}
 
 export const component = {
     name: 'modalImportModelsStepIndicator',
     template,
+    controller: ModalImportModelsStepIndicator,
     bindings: {
         steps: '<',
         currentStep: '<'
