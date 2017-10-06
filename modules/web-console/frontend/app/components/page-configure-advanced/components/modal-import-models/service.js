c3522200aa9bff40e4bda44879eb98986e61c4c0
Ilya Borisov <Klaster1@gmail.com>
2017-10-06 10:57:26 +0700

    update import modal
    1. Move clusterID to bindings.
    2. Use dynamic state to display the modal. It will trigger forms uiCanExit callback.

--- modules/web-console/frontend/app/components/page-configure-advanced/components/modal-import-models/service.js
+++ modules/web-console/frontend/app/components/page-configure-advanced/components/modal-import-models/service.js
@@ -2,21 +2,38 @@ export default class ModalImportModels {
     static $inject = ['$modal', 'AgentManager', '$uiRouter'];
     constructor($modal, AgentManager, $uiRouter) {
         Object.assign(this, {$modal, AgentManager, $uiRouter});
-        this.importDomainModal = this.$modal({
+    }
+    _goToDynamicState() {
+        if (this._state) this.$uiRouter.stateRegistry.deregister(this._state);
+        this._state = this.$uiRouter.stateRegistry.register({
+            name: 'importModels',
+            parent: this.$uiRouter.stateService.current,
+            onEnter: () => {
+                this._open();
+            },
+            onExit: () => {
+                this._modal && this._modal.hide();
+            }
+        });
+        return this.$uiRouter.stateService.go(this._state, this.$uiRouter.stateService.params);
+    }
+    _open() {
+        this._modal = this.$modal({
             template: `
                 <modal-import-models
-                    on-hide='$hide()'
+                    on-hide='$ctrl.$state.go("^")'
+                    cluster-id='$ctrl.$state.params.clusterID'
                 ></modal-import-models>
             `,
+            controller: ['$state', function($state) {this.$state = $state;}],
+            controllerAs: '$ctrl',
             show: false
         });
+        return this.AgentManager.startAgentWatch('Back', this.$uiRouter.globals.current.name)
+        .then(() => this._modal.$promise)
+        .then(() => this._modal.show());
     }
     open() {
-        return this.AgentManager.startAgentWatch('Back', this.$uiRouter.globals.current.name)
-        .then(() => this.importDomainModal.$promise)
-        .then((m) => {
-            this.importDomainModal.show();
-            m.locals.modalInstance = m;
-        });
+        this._goToDynamicState();
     }
 }
