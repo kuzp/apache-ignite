f9a66a5c86f430379a39767ec0d6759f5900edf6
Eduard Shangareev <eshangareev@gridgain.com>
2017-10-10 12:14:44 +0300

    ignite-gg-12912 snapshot refactoring for baseLineTopology

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cluster/BaselineTopology.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cluster/BaselineTopology.java
@@ -29,6 +29,8 @@ import java.util.Set;
 import org.apache.ignite.cluster.ClusterNode;
 import org.apache.ignite.internal.cluster.DetachedClusterNode;
 import org.apache.ignite.internal.cluster.NodeOrderComparator;
+import org.apache.ignite.internal.util.typedef.internal.CU;
+import org.apache.ignite.lang.IgnitePredicate;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -68,10 +70,18 @@ public class BaselineTopology implements Serializable {
      * @return Sorted list of baseline topology nodes.
      */
     public List<ClusterNode> createBaselineView(List<ClusterNode> aliveNodes) {
+        return createBaselineView(aliveNodes, null);
+    }
+
+    /**
+     * @param aliveNodes Sorted list of currently alive nodes.
+     * @return Sorted list of baseline topology nodes.
+     */
+    public List<ClusterNode> createBaselineView(List<ClusterNode> aliveNodes, IgnitePredicate<ClusterNode> nodeFilter)  {
         List<ClusterNode> res = new ArrayList<>(nodeMap.size());
 
         for (ClusterNode node : aliveNodes) {
-            if (nodeMap.containsKey(node.consistentId()))
+            if (nodeMap.containsKey(node.consistentId()) && (nodeFilter == null || CU.affinityNode(node, nodeFilter)))
                 res.add(node);
         }
 
@@ -83,7 +93,7 @@ public class BaselineTopology implements Serializable {
         Map<Object, ClusterNode> consIdMap = new HashMap<>();
 
         for (ClusterNode node : aliveNodes) {
-            if (nodeMap.containsKey(node.consistentId()))
+            if (nodeMap.containsKey(node.consistentId()) && (nodeFilter == null || CU.affinityNode(node, nodeFilter)))
                 consIdMap.put(node.consistentId(), node);
         }
 
@@ -93,7 +103,8 @@ public class BaselineTopology implements Serializable {
             if (!consIdMap.containsKey(consId)) {
                 DetachedClusterNode node = new DetachedClusterNode(consId, e.getValue());
 
-                consIdMap.put(consId, node);
+                if (nodeFilter == null || CU.affinityNode(node, nodeFilter))
+                    consIdMap.put(consId, node);
             }
         }
 
@@ -148,9 +159,8 @@ public class BaselineTopology implements Serializable {
 
         Map<Object, Map<String, Object>> nodeMap = new HashMap<>();
 
-        for (ClusterNode node : nodes) {
+        for (ClusterNode node : nodes)
             nodeMap.put(node.consistentId(), node.attributes());
-        }
 
         return new BaselineTopology(nodeMap);
     }
