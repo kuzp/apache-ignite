918fb6cb05ac142a7f2a201935222e062ac5711f
Ilya Borisov <Klaster1@gmail.com>
2017-10-27 16:40:35 +0700

    list-editable rebase

--- modules/web-console/frontend/app/components/list-editable/index.js
+++ modules/web-console/frontend/app/components/list-editable/index.js
@@ -20,10 +20,52 @@ import angular from 'angular';
 import component from './component';
 import listEditableCols from './components/list-editable-cols';
 import transclude from './components/list-editable-transclude';
+import listEditableOneWay from './components/list-editable-one-way';
+import {default as ListEditableController} from './controller';
+
+const CUSTOM_EVENT_TYPE = '$ngModel.change';
 
 export default angular
     .module('ignite-console.list-editable', [
         listEditableCols.name,
+        listEditableOneWay.name,
         transclude.name
     ])
-    .component('listEditable', component);
+    .component('listEditable', component)
+    // Emits $ngModel.change event on every ngModel.$viewValue change
+    .directive('ngModel', function() {
+        return {
+            /**
+             * @param {ng.INgModelController} ngModel
+             */
+            link(scope, el, attr, ngModel) {
+                ngModel.$viewChangeListeners.push(() => {
+                    el[0].dispatchEvent(new CustomEvent(CUSTOM_EVENT_TYPE, {bubbles: true, cancelable: true}));
+                });
+            },
+            require: 'ngModel'
+        };
+    })
+    // Triggers $ctrl.save when any ngModel emits $ngModel.change event
+    .directive('listEditableItemEdit', function() {
+        return {
+            /**
+             * @param {ListEditableController} list
+             */
+            link(scope, el, attr, list) {
+                if (!list) return;
+                let listener = (e) => {
+                    e.stopPropagation();
+                    scope.$evalAsync(() => {
+                        if (scope.$parent.form.$valid) list.save(scope.$parent.item, scope.$parent.$index);
+                    });
+                };
+                el[0].addEventListener(CUSTOM_EVENT_TYPE, listener);
+                scope.$on('$destroy', () => {
+                    el[0].removeEventListener(CUSTOM_EVENT_TYPE, listener);
+                    listener = null;
+                });
+            },
+            require: '?^listEditable'
+        };
+    });
