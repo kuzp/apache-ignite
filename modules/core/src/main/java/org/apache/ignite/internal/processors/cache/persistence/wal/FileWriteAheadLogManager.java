9d2d7b98f4b340bc156cde5ec67b3ef598a2eae4
Dmitriy Govorukhin <dmitriy.govorukhin@gmail.com>
2017-11-15 13:12:31 +0300

    ignite-2.4.1-truncate-wal remove val on snapshot move/delete

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/persistence/wal/FileWriteAheadLogManager.java
@@ -27,9 +27,11 @@ import java.nio.ByteOrder;
 import java.nio.file.Files;
 import java.sql.Time;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NavigableMap;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -231,6 +233,9 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
      * Should be cancelled at shutdown
      */
     @Nullable private volatile GridTimeoutObject nextAutoArchiveTimeoutObj;
+
+    /** RollOver set. */
+    private final Set<WALRecord.RecordType> rollOverTypes = Collections.singleton(WALRecord.RecordType.SNAPSHOT);
 
     /**
      * @param ctx Kernal context.
@@ -504,7 +509,10 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
         // Need to calculate record size first.
         record.size(serializer.size(record));
 
-        for (; ; currWrHandle = rollOver(currWrHandle)) {
+        while (true) {
+            if (rollOverTypes.contains(record.type()))
+                currWrHandle = rollOver(currWrHandle);
+
             WALPointer ptr = currWrHandle.addRecord(record);
 
             if (ptr != null) {
@@ -517,6 +525,8 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
 
                 return ptr;
             }
+            else
+                currWrHandle = rollOver(currWrHandle);
 
             checkEnvironment();
 
@@ -642,14 +652,15 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
     }
 
     /** {@inheritDoc} */
-    @Override public int truncate(WALPointer ptr) {
-        if (ptr == null)
+    @Override public int truncate(WALPointer from, WALPointer to) {
+        if (to == null)
             return 0;
 
-        assert ptr instanceof FileWALPointer : ptr;
+        assert to instanceof FileWALPointer : to;
 
         // File pointer bound: older entries will be deleted from archive
-        FileWALPointer fPtr = (FileWALPointer)ptr;
+        FileWALPointer fromPtr = (FileWALPointer)from;
+        FileWALPointer toPtr = (FileWALPointer)to;
 
         FileDescriptor[] descs = scan(walArchiveDir.listFiles(WAL_SEGMENT_FILE_FILTER));
 
@@ -658,12 +669,15 @@ public class FileWriteAheadLogManager extends GridCacheSharedManagerAdapter impl
         FileArchiver archiver0 = archiver;
 
         for (FileDescriptor desc : descs) {
+            if (fromPtr != null && desc.idx < fromPtr.index())
+                continue;
+
             // Do not delete reserved or locked segment and any segment after it.
             if (archiver0 != null && archiver0.reserved(desc.idx))
                 return deleted;
 
             // We need to leave at least one archived segment to correctly determine the archive index.
-            if (desc.idx + 1 < fPtr.index()) {
+            if (desc.idx + 1 < toPtr.index()) {
                 if (!desc.file.delete())
                     U.warn(log, "Failed to remove obsolete WAL segment (make sure the process has enough rights): " +
                         desc.file.getAbsolutePath());
