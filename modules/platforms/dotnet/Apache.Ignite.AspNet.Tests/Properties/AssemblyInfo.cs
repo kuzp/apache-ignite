5dbb14e563870205a5175e97f68a4696930f2a39
Ignite Teamcity <ignite@apache.org>
2017-11-20 12:21:25 +0000

    version changed to: 1.9.9

--- modules/platforms/dotnet/Apache.Ignite.AspNet.Tests/Properties/AssemblyInfo.cs
+++ modules/platforms/dotnet/Apache.Ignite.AspNet.Tests/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("18ea4c71-a11d-4ab1-8042-418f7559d84f")]
 
-[assembly: AssemblyVersion("2.0.0.16694")]
-[assembly: AssemblyFileVersion("2.0.0.16694")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("1.9.9.24564")]
+[assembly: AssemblyFileVersion("1.9.9.24564")]
+[assembly: AssemblyInformationalVersion("1.9.9")]
 
 [assembly: CLSCompliant(true)]
